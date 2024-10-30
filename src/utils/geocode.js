0047659400b26e02cc1ee964b916821684802457
unknown <yolalestari497@gmail.com>
2024-10-30 22:46:01 +0800

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('postman-request');
+
+const geocode = (address, callback) => {
+    const url = 'http://api.positionstack.com/v1/forward?access_key=123041402d0449cd63f9cb58c1955e53&query=' + 
+        encodeURIComponent(address);
+        
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Tidak dapat terkoneksi ke layanan', undefined);
+        } else if (!response.body.data || response.body.data.length === 0) {
+            callback('Tidak dapat menemukan lokasi. Lakukan pencarian lokasi yang lain', undefined);
+        } else {
+            callback(undefined, {
+                latitude: response.body.data[0].latitude,
+                longitude: response.body.data[0].longitude,
+                location: response.body.data[0].label
+            });
+        }
+    });
+};
